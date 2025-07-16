36fb60adc7984264e6834bfab529cc1dc0580b4f
Joona Gynther <joona@gynther.xyz>
2025-07-16 21:46:09 +0300

    switch to alejandra for formatting nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,56 +7,51 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nix-darwin,
-      nixpkgs,
-    }@inputs:
-    let
-      username = "gynther";
-      hostname = "MacBook-Air";
-      system = "aarch64-darwin";
-      specialArgs = { inherit inputs username hostname; };
+  outputs = {
+    self,
+    nix-darwin,
+    nixpkgs,
+  } @ inputs: let
+    username = "gynther";
+    hostname = "MacBook-Air";
+    system = "aarch64-darwin";
+    specialArgs = {inherit inputs username hostname;};
 
-      configuration =
-        { pkgs, ... }:
-        {
-          nix.enable = false; # Determinate Nix specific
-          nix.settings.experimental-features = "nix-command flakes";
+    configuration = {pkgs, ...}: {
+      nix.enable = false; # Determinate Nix specific
+      nix.settings.experimental-features = "nix-command flakes";
 
-          environment.variables = {
-            NH_FLAKE = "/etc/nix-darwin";
-          };
-
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 6;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = system;
-
-          users.users.${username} = {
-            home = "/Users/${username}";
-          };
-
-          # https://discussions.apple.com/thread/255187302?sortBy=rank
-          security.pam.services.sudo_local.touchIdAuth = true;
-
-          environment.systemPackages = with pkgs; [
-            nixd
-            nixfmt-rfc-style
-            just
-            nh
-          ];
-        };
-    in
-    {
-      darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-        inherit system specialArgs;
-        modules = [ configuration ];
+      environment.variables = {
+        NH_FLAKE = "/etc/nix-darwin";
       };
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = system;
+
+      users.users.${username} = {
+        home = "/Users/${username}";
+      };
+
+      # https://discussions.apple.com/thread/255187302?sortBy=rank
+      security.pam.services.sudo_local.touchIdAuth = true;
+
+      environment.systemPackages = with pkgs; [
+        nixd
+        just
+        nh
+        alejandra # nixfmt-rfc-style
+      ];
     };
+  in {
+    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
+      inherit system specialArgs;
+      modules = [configuration];
+    };
+  };
 }
